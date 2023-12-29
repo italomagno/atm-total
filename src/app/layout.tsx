e2a8d4202b37da53609ef42853bbdd3f7c8e7d56
italo1~ <italomagno10@gmail.com1~>
2023-12-29 11:33:19 -0300

    adding analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import { Analytics } from '@vercel/analytics/react';
 import { Inter } from 'next/font/google'
 import '@/styles/globals.css'
 import { DirectoryProvider } from '@/providers/DirectoryProvider'
@@ -22,6 +23,7 @@ export default function RootLayout({
         <ModalProvider>
         <DirectoryProvider>
           {children}
+          <Analytics />
         </DirectoryProvider>
         </ModalProvider>
       </body>
